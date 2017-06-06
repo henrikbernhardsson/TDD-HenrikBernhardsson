3af192901ba3746eeae42783201b34d58272b090
henrikbernhardsson <henke.ber@gmail.com>
2017-06-06 22:29:36 +0200

    Done

--- TDDExcercises/TravelAgency2.Tests/TourScheduleTests.cs
+++ TDDExcercises/TravelAgency2.Tests/TourScheduleTests.cs
@@ -17,46 +17,46 @@ namespace TravelAgency2.Tests
         [Test]
         public void CanCreateNewTour()
         {
-            _sut.CreateTour("New years day safari", new DateTime(2013, 1, 1), 20);
-            var result = _sut.GetToursFor(new DateTime(2013, 1, 1));
+            _sut.CreateTour("Madagaskar boat trip", new DateTime(2017, 10, 22), 44);
+            var result = _sut.GetToursFor(new DateTime(2017, 10, 22));
 
             Assert.AreEqual(1, result.Count);
-            Assert.AreEqual("New years day safari", result[0].Name);
-            Assert.AreEqual(20, result[0].NumberOfSeats);
+            Assert.AreEqual("Madagaskar boat trip", result[0].Name);
+            Assert.AreEqual(44, result[0].NumberOfSeats);
 
         }
 
         [Test]
         public void ToursAreScheduledByDateOnly()
         {
-            _sut.CreateTour("New years day safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
-            var result = _sut.GetToursFor(new DateTime(2013, 1, 1));
+            _sut.CreateTour("Madagaskar boat trip", new DateTime(2017, 10, 22), 44);
+            var result = _sut.GetToursFor(new DateTime(2017, 10, 22));
 
-            Assert.AreEqual(new DateTime(2013, 1, 1), result[0].Date.Date);
+            Assert.AreEqual(new DateTime(2017, 10, 22), result[0].Date.Date);
         }
 
         [Test]
         public void GetToursForGivenDayOnly()
         {
-            _sut.CreateTour("New years day safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
-            _sut.CreateTour("May safari", new DateTime(2013, 5, 1, 10, 15, 0), 20);
-            _sut.CreateTour("July safari", new DateTime(2013, 7, 1, 10, 15, 0), 20);
-            _sut.CreateTour("April safari", new DateTime(2013, 4, 1, 10, 15, 0), 20);
+            _sut.CreateTour("Madagaskar boat trip", new DateTime(2017, 10, 22), 44);
+            _sut.CreateTour("New years day safari", new DateTime(2017, 01, 01), 44);
+            _sut.CreateTour("Christmas Safari", new DateTime(2017, 12, 25), 44);
 
-            var result = _sut.GetToursFor(new DateTime(2013, 4, 1));
-
-            Assert.AreEqual(new DateTime(2013, 4, 1), result[0].Date.Date);
+            var result = _sut.GetToursFor(new DateTime(2017, 01, 01));
+           
+            Assert.AreEqual(new DateTime(2017, 01, 01), result[0].Date.Date);
             Assert.AreEqual(1, result.Count);
         }
 
         [Test]
         public void ThrowsExceptionWhenThereIsMoreThanOneTourPerDay()
         {
-            _sut.CreateTour("New years day safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
-            _sut.CreateTour("January safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
-            _sut.CreateTour("Winter safari", new DateTime(2013, 1, 1, 10, 15, 0), 20);
+            _sut.CreateTour("Madagaskar boat trip", new DateTime(2017, 10, 22), 44);
+            _sut.CreateTour("New years day safari", new DateTime(2017, 10, 22), 44);
+            _sut.CreateTour("Christmas Safari", new DateTime(2017, 10, 22), 44);
 
-            Assert.Throws<TourAllocationException>(() => _sut.CreateTour("Winter safari", new DateTime(2013, 1, 1, 10, 15, 0), 20));
+            Assert.Throws<TourAllocationException>(() =>
+            _sut.CreateTour("Boat trip to europe", new DateTime(2017, 10, 22), 44));
         }
     }
 }
